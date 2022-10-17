bd3ea2b83fcf3cfae4dea933be8049c4c4d356b5
Gregory <adrian0808.jesus@gmail.com>
2022-10-17 11:46:31 -0300

    struturas (structs) C++

--- struturas (structs) C++.cpp
+++ struturas (structs) C++.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 //struct para frutas
-struct fruta{
+struct Fruta{
  string cor;
  string nome;
 };
@@ -16,13 +16,32 @@ struct fruta{
 int main(){
 
 //Criando um ponteiro para o struct criado
-  fruta *primeiraFruta = new fruta;
+  Fruta *primeiraFruta = new Fruta;
 
 //modificando valores
 primeiraFruta->cor = "Amarelo";
 primeiraFruta->nome = "Banana\n";
 
 cout << "Fruta:" << primeiraFruta->nome <<"cor: " << primeiraFruta->cor;
+
+//criando uma lista de frutas
+
+ Fruta *ponteiroListaDeFrutas = new Fruta[2];
+
+ //modifcando ponteiro
+ ponteiroListaDeFrutas[0].cor = "Vermelho";
+ ponteiroListaDeFrutas[0].nome = "Morango";
+ ponteiroListaDeFrutas[1].cor = "Verde";
+ ponteiroListaDeFrutas[1].nome = "Melancia";
+
+ //percorrendo e exibindo
+ int cont;
+ for(cont = 0; cont < 2; cont++){
+
+cout << "\nFruta:" << ponteiroListaDeFrutas[cont].nome << "\ncor: " << ponteiroListaDeFrutas[cont].cor;
+
+ }
+
 
 return 0;
 }
