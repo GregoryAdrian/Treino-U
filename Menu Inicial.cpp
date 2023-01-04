c107afc2385eb99ad1e6246d15f4aff99f6511d8
Gregory Adrian <adrian0808.jesus@gmail.com>
2023-01-04 14:27:53 -0300

    Menu-Inicial

--- Menu Inicial.cpp
+++ Menu Inicial.cpp
@@ -3,12 +3,23 @@
 # include <string>
 # include <stdlib>
 
-
+using namespace std;
 
 int main (){
 
-cout <<
+ //variaveis
+ cout << "Operações \n";
+ cout << " 1 - Insercao de um node no inicio da lista";
+ cout << " 2 - Insercao de um node no fim da lista \n";
+ cout << " 3 - Insercao de um node na posição N \n";
+ cout << " 4 - retirar  um node no inicio da lista";
+ cout << " 5 - Retirar  um node no fim da lista \n";
+ cout << " 6 - Retirar  um node na posição N \n";
+ cout << " 7 - procurar um node com o campo RG \n";
+ cout << " 8 - Imprimir a Lista. \n";
+ cout << " 9 - Imprimir do sistema. \n";
+ cout << " \n Escolha um numero e pressione ENTER: \n";
 
 
-return 0
+return 0;
 }
