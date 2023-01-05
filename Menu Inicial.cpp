926d31e0a2122f34d477ee1ed76365eae0d88654
Gregory Adrian <adrian0808.jesus@gmail.com>
2023-01-05 18:05:13 -0300

    Menu Inicial

--- Menu Inicial.cpp
+++ Menu Inicial.cpp
@@ -1,15 +1,24 @@
-# include <iostream>
-# include <new>
-# include <string>
-# include <stdlib>
+#include <iostream>
+#include <new>
+#include <string>
+#include <stdlib.h>
 
 using namespace std;
 
+void limpaTela(){
+ system("CLS");
+}
+
 int main (){
 
- //variaveis
- cout << "Operações \n";
- cout << " 1 - Insercao de um node no inicio da lista";
+int funcaoDesejada = 1;
+
+
+while(funcaoDesejada < 10 && funcaoDesejada > 0){
+
+        //Mostrando o menu
+ cout << " Operacoes \n";
+ cout << " 1 - Insercao de um node no inicio da lista \n";
  cout << " 2 - Insercao de um node no fim da lista \n";
  cout << " 3 - Insercao de um node na posição N \n";
  cout << " 4 - retirar  um node no inicio da lista";
@@ -20,6 +29,25 @@ int main (){
  cout << " 9 - Imprimir do sistema. \n";
  cout << " \n Escolha um numero e pressione ENTER: \n";
 
+     // Lendo a opção do usuario
+  cin >> funcaoDesejada;
+
+  limpaTela();
+
+  //Chama a função desejada
+  switch(funcaoDesejada){
+      case 1:
+           cout << "Funcao escolhida: 1 \n";
+        break;
+
+         case 2:
+           cout << "Funcao escolhida: 2 \n";
+        break;
+
+
+  }
+
+}
 
 return 0;
 }
